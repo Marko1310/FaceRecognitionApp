5861610a6c7769e0d7ddd723c7324e60c3bfd84b
Marko1310 <marko.cabo13@gmail.com>
2023-01-14 15:38:03 +0100

    Implemented parallax tilt component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,5 +1,22 @@
+import React from "react";
+import Tilt from "react-parallax-tilt";
+
 const Logo = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Tilt>
+        <div
+          style={{
+            width: "200px",
+            height: "300px",
+            backgroundColor: "darkgreen",
+          }}
+        >
+          <h1>React Parallax Tilt 👀</h1>
+        </div>
+      </Tilt>
+    </div>
+  );
 };
 
 export default Logo;
