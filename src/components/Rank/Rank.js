0d79f8166d4bf8fbf7acebd92c8fa2184c4ab9da
Marko1310 <marko.cabo13@gmail.com>
2023-01-14 18:03:35 +0100

    Structure of Rank component completed

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Rank.css";
+
+const Rank = () => {
+  return (
+    <div className="rank-container">
+      <div className="rank-text">{`Your rank is...`}</div>
+      <div className="rank-number">{`#5`}</div>
+    </div>
+  );
+};
+
+export default Rank;
